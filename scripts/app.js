f48e0708b77f27a04aec48a2c42a2392d0bc0a0e
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-11 00:09:17 +0200

    about us

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,24 @@
 
 function toggleDropdown(id) {
     var dropdown = document.getElementById(id);
-    dropdown.style.display = dropdown.style.display === "none" ? "block" : "none";
+    var dropdowns = document.querySelectorAll('.dropdown-content');
+    
+    dropdowns.forEach(function(item) {
+      if (item.id !== id) {
+        item.classList.remove('active');
+      }
+    });
+
+    dropdown.classList.toggle('active');
   }
+
+  //our team
+
+
+const cards = document.querySelectorAll(".grid-item");
+cards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    cards.forEach((el) => el.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
